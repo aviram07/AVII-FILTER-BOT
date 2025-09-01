609a8fee96d523e1f48d38cadd5a53154530362e
aviram07 <avirammahalik818@gmail.com>
2025-09-01 22:43:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,8 +11,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '23608041'))
+API_HASH = environ.get('API_HASH', '43cbb86bd64ed7ca6c2f2f329a94966a')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
@@ -196,3 +196,4 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
